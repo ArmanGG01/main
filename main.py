5990061d0e6cc72d8ec7726797cc31fd979c2d11
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2023-08-31 05:10:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,22 @@ import telebot
 # Ganti 'YOUR_BOT_TOKEN' dengan token bot Anda
 bot = telebot.TeleBot('6396387965:AAHX5skZodxk19t44JztLMEwQxk-l0Qyznk')
 
-@bot.message_handler(commands=['start', 'help'])
 def start(message):
     bot.send_message(message.chat.id, "Halo! Aku adalah bot gabut. Kirimkan pesan apapun dan kita bisa ngobrol santai!")
 
-@bot.message_handler(func=lambda message: True)
 def handle_message(message):
     if message.text.lower() in ['halo', 'hi', 'hai']:
-        bot.send_message(message.chat.id, "Halo! Apa kabar?")
+        user_name = message.from_user.first_name
+        response = f"Halo {user_name}! Apa kabar?"
+        bot.send_message(message.chat.id, response)
     else:
         bot.send_message(message.chat.id, "Hmmm... Aku juga tidak terlalu tahu apa yang harus kukatakan. 🤷‍♂️")
+
+def on_start_command(message):
+    start(message)
+
+def on_any_message(message):
+    handle_message(message)
 
 # Jalankan bot
 bot.polling()
