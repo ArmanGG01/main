8ed117bc904535d36a07a5557359acf62ac53e5e
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2023-08-31 05:27:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,26 @@ import telebot
 bot = telebot.TeleBot('6396387965:AAHVSjgaqZPYZ6U4tdceG_-b2CEzDi2EtG8')
 
 def start(message):
-    bot.send_message(message.chat.id, "Halo! Aku adalah bot gabut. Kirimkan pesan apapun dan kita bisa ngobrol santai!")
+    bot.send_message(message.chat.id, "Halo! Aku adalah bot gabut yang di ciptakan oleh Arman. Kirimkan pesan apapun dan kita bisa ngobrol santai!")
 
 def handle_message(message):
-    if message.text.lower() in ['halo', 'hi', 'hai']:
-        user_name = message.from_user.first_name
+    user_input = message.text.lower()
+    user_name = message.from_user.first_name
+    
+    if user_input in ['halo', 'hi', 'hai']:
         response = f"Halo {user_name}! Apa kabar?"
-        bot.send_message(message.chat.id, response)
+    elif user_input == 'apa kabar?':
+        response = f"Aku hanya bot, tapi aku baik-baik saja, {user_name}."
+    elif user_input == 'siapa kamu?':
+        response = "Aku adalah bot gabut, tidak punya identitas yang jelas. 😄"
+    elif user-input == 'kenalan':
+        response = "perkenalkan nama saya Bot Gabut, lalu nama kamu siapa?"
+    elif user_input == 'nama':
+        response = f"senang berkenalan dengan {user_name} Mu 😊."
     else:
-        bot.send_message(message.chat.id, "Hmmm... Aku juga tidak terlalu tahu apa yang harus kukatakan. 🤷‍♂️")
+        response = f"Hmmm... Aku juga tidak terlalu tahu apa yang harus kukatakan, {user_name}. 🤷‍♂️"
+        
+    bot.send_message(message.chat.id, response)
 
 @bot.message_handler(commands=['start', 'help'])
 def on_start_command(message):
