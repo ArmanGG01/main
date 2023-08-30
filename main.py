fdd8b6b8bd20162ee290ab97efb9488cdba6373a
ArmanGG01 <96017229+ArmanGG01@users.noreply.github.com>
2023-08-31 04:22:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,32 @@ import random
 TOKEN = '6396387965:AAHIUuWxTJMfYCrATSXMZOf1a5OI6_km0AM'
 bot = telebot.TeleBot(TOKEN)
 
-# Keyboard kustom
-keyboard = types.ReplyKeyboardMarkup(row_width=2)
+# Keyboard kustom lebih kecil
+keyboard = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
 start_button = types.KeyboardButton('/start')
 help_button = types.KeyboardButton('/help')
 keyboard.add(start_button, help_button)
+
+# Daftar link gambar acak
+image_links = [
+    "https://link_gambar1.jpg",
+    "https://link_gambar2.jpg",
+    # Tambahkan link gambar lainnya di sini
+]
+
+# Daftar fakta menarik
+facts = [
+    "Fakta menarik 1.",
+    "Fakta menarik 2.",
+    # Tambahkan fakta lainnya di sini
+]
+
+# Daftar lelucon lucu
+jokes = [
+    "Dua Tiga Tutup Botol Mukak Kau Macam Kontol",
+    "Btw Aku Sayang Kalian ",
+    # Tambahkan lelucon lainnya di sini
+]
 
 # Perintah start
 @bot.message_handler(commands=['start'])
@@ -30,28 +51,20 @@ def send_help(message):
 # Perintah gambar
 @bot.message_handler(commands=['gambar'])
 def send_image(message):
-    images = ["link_gambar1.jpg", "link_gambar2.jpg"]  # Ganti dengan tautan gambar yang sesuai
-    random_image = random.choice(images)
+    random_image = random.choice(image_links)
     bot.send_photo(message.chat.id, photo=random_image)
 
 # Perintah fakta
 @bot.message_handler(commands=['fakta'])
 def send_fact(message):
-    facts = ["Fakta menarik 1.", "Fakta menarik 2.", "Fakta menarik 3."]
     random_fact = random.choice(facts)
     bot.reply_to(message, random_fact)
 
 # Perintah lelucon
 @bot.message_handler(commands=['lelucon'])
 def send_joke(message):
-    jokes = ["Lelucon lucu 1.", "Lelucon lucu 2.", "Lelucon lucu 3."]
     random_joke = random.choice(jokes)
     bot.reply_to(message, random_joke)
-
-# Tanggapan berdasarkan kata kunci
-@bot.message_handler(func=lambda message: 'lucu' in message.text.lower())
-def respond_to_keyword(message):
-    bot.reply_to(message, "Hehe, memang lucu ya!")
 
 # Menjalankan bot
 bot.polling()
